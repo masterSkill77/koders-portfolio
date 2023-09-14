de4cf28b8553baa37b388982dcb49a46a21b5a6e
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-09-14 15:48:24 +0300

    feat : add component and layout for SPA page

--- app.vue
+++ app.vue
@@ -1,3 +1,6 @@
+<script setup></script>
 <template>
-	<div></div>
+	<NuxtLayout>
+		<LazyNuxtPage />
+	</NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		Bonjour
+		<slot />
+	</div>
+</template>
