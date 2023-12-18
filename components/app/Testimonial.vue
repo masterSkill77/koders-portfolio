76f4be90056c15b10b6a6d77359c564aed72f679
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-12-18 21:46:18 +0300

    feat : get testimonial from api

--- components/app/Testimonial.vue
+++ components/app/Testimonial.vue
@@ -1,80 +1,58 @@
+<script setup>
+	import { register } from 'swiper/element/bundle';
+	// register Swiper custom elements
+	register();
+	const testimonials = ref([]);
+	onMounted(async () => {
+		const data = await useFetchData('/testimonials');
+		testimonials.value = data;
+	});
+</script>
 <template>
-	<div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+	<div class="container-fluid py-5">
 		<div class="container py-5">
 			<div
 				class="section-title text-center position-relative pb-3 mb-4 mx-auto"
 				style="max-width: 600px">
-				<h5 class="fw-bold text-primary text-uppercase">Testimonial</h5>
-				<h1 class="mb-0">What Our Clients Say About Our Digital Services</h1>
+				<h5 class="fw-bold text-primary text-uppercase">Témoignages</h5>
+				<h1 class="mb-0">Ce que nos collaborateurs pensent de nous</h1>
 			</div>
-			<div
-				class="owl-carousel testimonial-carousel wow fadeInUp"
-				data-wow-delay="0.6s">
-				<div class="testimonial-item bg-light my-4">
-					<div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
-						<img
-							class="img-fluid rounded"
-							src="img/testimonial-1.jpg"
-							style="width: 60px; height: 60px" />
-						<div class="ps-4">
-							<h4 class="text-primary mb-1">Client Name</h4>
-							<small class="text-uppercase">Profession</small>
+			<div class="testimonial-carousel">
+				<swiper-container
+					slides-per-view="3"
+					:autoplay="{ delay: 5000 }"
+					speed="2000"
+					loop="true">
+					<swiper-slide
+						v-for="testimonial in testimonials"
+						:key="testimonial.id"
+						class="testimonial-item">
+						<div class="testimonial__item">
+							<div
+								class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+								<img
+									class="img-fluid rounded"
+									:src="testimonial.photo"
+									style="width: 60px; height: 60px; object-fit: cover" />
+								<div class="ps-4">
+									<h4 class="text-primary mb-1">{{ testimonial.client }}</h4>
+									<small class="text-uppercase">{{ testimonial.title }}</small>
+								</div>
+							</div>
+							<div
+								class="pt-4 pb-5 px-5"
+								v-html="testimonial.description"></div>
 						</div>
-					</div>
-					<div class="pt-4 pb-5 px-5">
-						Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet
-						amet eirmod eos labore diam
-					</div>
-				</div>
-				<div class="testimonial-item bg-light my-4">
-					<div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
-						<img
-							class="img-fluid rounded"
-							src="img/testimonial-2.jpg"
-							style="width: 60px; height: 60px" />
-						<div class="ps-4">
-							<h4 class="text-primary mb-1">Client Name</h4>
-							<small class="text-uppercase">Profession</small>
-						</div>
-					</div>
-					<div class="pt-4 pb-5 px-5">
-						Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet
-						amet eirmod eos labore diam
-					</div>
-				</div>
-				<div class="testimonial-item bg-light my-4">
-					<div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
-						<img
-							class="img-fluid rounded"
-							src="img/testimonial-3.jpg"
-							style="width: 60px; height: 60px" />
-						<div class="ps-4">
-							<h4 class="text-primary mb-1">Client Name</h4>
-							<small class="text-uppercase">Profession</small>
-						</div>
-					</div>
-					<div class="pt-4 pb-5 px-5">
-						Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet
-						amet eirmod eos labore diam
-					</div>
-				</div>
-				<div class="testimonial-item bg-light my-4">
-					<div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
-						<img
-							class="img-fluid rounded"
-							src="img/testimonial-4.jpg"
-							style="width: 60px; height: 60px" />
-						<div class="ps-4">
-							<h4 class="text-primary mb-1">Client Name</h4>
-							<small class="text-uppercase">Profession</small>
-						</div>
-					</div>
-					<div class="pt-4 pb-5 px-5">
-						Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet
-						amet eirmod eos labore diam
-					</div>
-				</div>
+					</swiper-slide>
+				</swiper-container>
 			</div>
 		</div>
 	</div>
 </template>
+<style scoped class="testimonial-item">
+	.testimonial-item {
+		width: 400px !important;
+		margin: 20px !important;
+		box-shadow: #0000004a 6px 5px 20px;
+	}
+</style>
