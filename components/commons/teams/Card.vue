3fb0c2ef143a41a2f9bef805479f4750c6afd288
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-12-18 21:46:28 +0300

    feat : update teams card

--- components/commons/teams/Card.vue
+++ components/commons/teams/Card.vue
@@ -1,8 +1,9 @@
 <script setup>
-	const { title, description } = defineProps({
+	const { title, description, image, slogan } = defineProps({
 		name: { type: String, required: false },
-		designation: { type: String, required: false },
+		designation: { type: Array, required: false },
 		image: { type: String, required: false },
+		slogan: { type: String, required: false },
 	});
 </script>
 <template>
@@ -10,23 +11,13 @@
 		<div class="team-img position-relative overflow-hidden">
 			<img class="img-fluid w-100" :src="image" loading="lazy" alt="" />
 			<div class="team-social">
-				<a class="btn btn-lg btn-primary btn-lg-square rounded" href=""
-					><i class="fab fa-twitter fw-normal"></i
-				></a>
-				<a class="btn btn-lg btn-primary btn-lg-square rounded" href=""
-					><i class="fab fa-facebook-f fw-normal"></i
-				></a>
-				<a class="btn btn-lg btn-primary btn-lg-square rounded" href=""
-					><i class="fab fa-instagram fw-normal"></i
-				></a>
-				<a class="btn btn-lg btn-primary btn-lg-square rounded" href=""
-					><i class="fab fa-linkedin-in fw-normal"></i
-				></a>
+				<a class="btn text-white rounded" href="">" {{ slogan }} " </a>
 			</div>
 		</div>
 		<div class="text-center p-2">
 			<h4 class="text-primary">{{ name }}</h4>
-			<p class="text-uppercase m-0">{{ designation }}</p>
+			<hr />
+			<p class="text-uppercase m-0">{{ designation.join(' | ') }}</p>
 		</div>
 	</div>
 </template>
@@ -37,6 +28,7 @@
 		object-fit: cover;
 	}
 	.team-item {
-		height: 500px !important;
+		height: 550px !important;
+		/* width: 300px; */
 	}
 </style>
