b31c92ebe09c97583dc1063fbf3e783dfd7caff3
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-12-18 21:45:32 +0300

    feat : use $fetch on useFetchData

--- composables/useFetchData.js
+++ composables/useFetchData.js
@@ -1,14 +1,3 @@
-// import { useAuth } from './../stores/useAuth';
-
-export const useFetchData = async (request, options) => {
-	return await useFetch(request, {
-		baseURL: import.meta.env.VITE_API_BASE_URL,
-		...options,
-		onRequest({ request, options }) {
-			options.headers = options.headers || {};
-			if (process.client) {
-				options.headers.accept = 'application/json';
-			}
-		},
-	});
+export const useFetchData = async (request) => {
+	return await $fetch(import.meta.env.VITE_API_BASE_URL + request);
 };
