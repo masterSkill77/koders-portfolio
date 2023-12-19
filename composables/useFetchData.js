743acff7c3a960a6395afe7bc3505dbcafaddf62
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-12-19 10:13:06 +0300

    fix : provide directly the baseURL in code

--- composables/useFetchData.js
+++ composables/useFetchData.js
@@ -1,3 +1,4 @@
+const baseURL = 'https://gestion.koders.mg/wp-json/api/v1';
 export const useFetchData = async (request) => {
-	return await $fetch(import.meta.env.VITE_API_BASE_URL + request);
+	return await $fetch(baseURL + request);
 };
